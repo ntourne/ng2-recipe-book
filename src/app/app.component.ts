53c9070138e6226b6d9ce30fef9db1acec87e6a5
Nicolas Tourne <ntourne@beerealit.com>
2016-10-02 19:01:14 -0300

    Add header with bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'rb-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  directives: [HeaderComponent]
 })
 export class AppComponent {
 }

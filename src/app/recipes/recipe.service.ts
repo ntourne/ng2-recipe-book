65e6fea0f263a98dbdfa34f01b07295c4378c0c6
Nicolas Tourne <ntourne@beerealit.com>
2016-10-03 16:26:34 -0300

    Shopping List service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipe';
+import { Ingredient } from '../shared/ingredient';
 
 @Injectable()
 export class RecipeService {
 
     private recipes: Recipe[] = [
-        new Recipe('Recipe 1', 'Recipe 1', 'http://images.media-allrecipes.com/userphotos/250x250/00/69/35/693521.jpg', []),
+        new Recipe('Recipe 1', 'Recipe 1', 'http://images.media-allrecipes.com/userphotos/250x250/00/69/35/693521.jpg', [
+            new Ingredient('French Fries', 2),
+            new Ingredient('Pork Meat', 1)
+        ]),
         new Recipe('Recipe 2', 'Recipe 2', 'http://www.simplebites.net/wp-content/uploads/2012/01/orzo-ingredients.jpg', []),
         new Recipe('Recipe 3', 'Recipe 3', 'http://topinspired.com/wp-content/uploads/2013/08/healthy-food-recipes-for-kids_01.jpg', [])
     ];
@@ -15,5 +19,5 @@ export class RecipeService {
     getRecipes() {
         return this.recipes;
     }
-    
+
 }

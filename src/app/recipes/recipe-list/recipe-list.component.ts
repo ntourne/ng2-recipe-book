36e30b20a3d4f3bb1816f0fb7eb838541f34c37e
Nicolas Tourne <ntourne@beerealit.com>
2016-10-02 21:41:15 -0300

    Add ngIf and ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,9 +7,12 @@ import { Recipe } from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
 
-    recipes: Recipe[] = [];
+    recipes: Recipe[] = [
+        new Recipe('Recipe 1', 'Recipe 1', 'http://images.media-allrecipes.com/userphotos/250x250/00/69/35/693521.jpg', []),
+        new Recipe('Recipe 2', 'Recipe 2', 'http://www.simplebites.net/wp-content/uploads/2012/01/orzo-ingredients.jpg', []),
+        new Recipe('Recipe 3', 'Recipe 3', 'http://topinspired.com/wp-content/uploads/2013/08/healthy-food-recipes-for-kids_01.jpg', [])
+    ];
     @Output() recipeSelected = new EventEmitter<Recipe>();
-    dummyRecipe = new Recipe('Dummy', 'Dummy', 'http://guides.global/images/guides/global/dummy_web_page.jpg');
 
     constructor() { }
 

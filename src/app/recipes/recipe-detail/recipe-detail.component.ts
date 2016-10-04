5d187892e28fd2a9e6ff24133f3365e210c9895a
Nicolas Tourne <ntourne@beerealit.com>
2016-10-04 13:45:45 -0300

    Edit and delete recipes

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
 import { Recipe } from '../recipe';
 import { ShoppingListService } from '../../shopping-list/shopping-list.service';
@@ -17,7 +17,8 @@ export class RecipeDetailComponent implements OnInit, OnDestroy {
 
     constructor(private sls: ShoppingListService,
                 private route: ActivatedRoute,
-                private recipeService: RecipeService) { }
+                private recipeService: RecipeService,
+                private router: Router) { }
 
     ngOnInit() {
         this.subscription = this.route.params.subscribe(
@@ -30,6 +31,15 @@ export class RecipeDetailComponent implements OnInit, OnDestroy {
 
     onAddToShoppingList() {
         this.sls.addItems(this.selectedRecipe.ingredients);
+    }
+
+    onEdit() {
+        this.router.navigate(['/recipes', this.recipeIndex, 'edit']);
+    }
+
+    onDelete() {
+        this.recipeService.deleteRecipe(this.selectedRecipe);
+        this.router.navigate(['/recipes']);
     }
 
     ngOnDestroy() {
